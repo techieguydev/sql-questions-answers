d4d87a87027185762085ece49f9bf4aa33d23465
Qadir Hussain <qadir.hussain99@gmail.com>
2025-05-28 22:08:54 +0100

    Top 10 SQL Interview queries: Find distance between cities

--- top-10-sql-interview-queries/cities_distance.sql
+++ top-10-sql-interview-queries/cities_distance.sql
@@ -15,5 +15,22 @@ INSERT INTO public.cities_distance(id, city1, city2, distance) VALUES (4, 'City 
 INSERT INTO public.cities_distance(id, city1, city2, distance) VALUES (5, 'City E', 'City F', 400);
 INSERT INTO public.cities_distance(id, city1, city2, distance) VALUES (6, 'City F', 'City E', 400);
 
+SELECT * FROM public.cities_distance;
 
-
+-- Query to find the distance between two cities
+-- Solution 1:
+WITH cte AS (
+    SELECT
+        city1,
+        city2,
+        distance,
+        ROW_NUMBER() OVER () AS rn
+    FROM public.cities_distance)
+SELECT 
+    c1.city1 AS city_from,
+    c1.city2 AS city_to,
+    c1.distance
+FROM cte c1
+LEFT JOIN cte c2 ON c1.city1 = c2.city2 AND c1.city2 = c2.city1
+WHERE c1.rn < c2.rn
+ORDER BY c1.city1;
