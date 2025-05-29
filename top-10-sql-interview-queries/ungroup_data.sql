053d1ea2a4a1a1cb01a023dc103e9a57bbd9d6dd
Qadir Hussain <qadir.hussain99@gmail.com>
2025-05-29 23:06:12 +0100

    Top 10 SQL Interview queries: Ungroup given data - more solution

--- top-10-sql-interview-queries/ungroup_data.sql
+++ top-10-sql-interview-queries/ungroup_data.sql
@@ -28,7 +28,21 @@ SELECT id, product, sales_count AS sales
 FROM ungrouped_sales
 ORDER BY sales_count;
 
--- Solution 2: Using a numbers table to ungroup data
+-- Solution 2: Using recursive CTE with a limit
+WITH RECURSIVE cte AS (
+    SELECT id, product, sales, 1 as total_count
+    FROM public.sales_data
+    UNION ALL
+    SELECT cte.id, cte.product, cte.sales - 1, total_count + 1
+    FROM cte
+    JOIN public.sales_data sd on sd.product = cte.product
+    AND sd.id = cte.id
+    WHERE cte.sales > 1)
+SELECT id, product, total_count AS sales
+FROM cte
+ORDER BY id, sales;
+
+-- Solution 3: Using a numbers table to ungroup data
 WITH numbers AS (
     SELECT generate_series(1, (SELECT MAX(sales) FROM public.sales_data)) AS n
 )
@@ -36,3 +50,12 @@ SELECT sd.id, sd.product, n.n AS sales
 FROM public.sales_data sd
 JOIN numbers n ON n.n <= sd.sales
 ORDER BY sd.id, n.n;
+
+-- Solution 4: Using a lateral join to ungroup data
+SELECT sd.id, sd.product, n.n AS sales
+FROM public.sales_data sd,
+LATERAL (
+    SELECT generate_series(1, sd.sales) AS n
+) AS n
+ORDER BY sd.id, n.n;
+
